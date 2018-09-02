7e6550c6e3423e958c66f2db389060cf46c32fa0
Kiều Trí Đăng <kieutridang@gmail.com>
2018-09-02 21:08:26 +0700

    append try catch for error on resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,21 +3,33 @@ const { User } = require('./schema');
 const resolvers = {
   Query: {
     user: async (_, { id }) => {
-      const user = await User.findById(id)
-      return user
+      try {
+        const user = await User.findById(id)
+        return user
+      } catch (error) {
+        console.log(`${__dirname}/query/user: `, error);
+      }
     },
     users: async () => {
-      const users = await User.findAll()
-      return users;
+      try {
+        const users = await User.findAll()
+        return users;
+      } catch (error) {
+        console.log(`${__dirname}/query/users: `, error);
+      }
     }
   },
   Mutation: {
     user: async (_, args) => {
-      const newUser = await User.create({
-        username: args.username,
-        password: args.password,
-      })
-      return newUser
+      try {
+        const newUser = await User.create({
+          username: args.username,
+          password: args.password,
+        })
+        return newUser
+      } catch (error) {
+        console.log(`${__dirname}/mutation/user: `, error);
+      }
     }
   } 
 }
